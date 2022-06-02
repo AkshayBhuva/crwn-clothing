3c177bc633be7e82300db7893e321e03603f3e8b
Akshay Bhuva <akshay.bhuva777@gmail.com>
2022-06-02 16:29:34 +1200

    crated authentication page component alongside sign in form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import Button from '../button/button.component';
 import FormInput from '../form-input/form-input.component';
-import './sign-up.styles.scss';
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
    displayName: '',
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,14 @@ import { async } from '@firebase/util';
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+   getAuth,
+   signInWithRedirect,
+   signInWithPopup,
+   GoogleAuthProvider,
+   createUserWithEmailAndPassword,
+   signInWithEmailAndPassword,
+} from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -62,4 +69,10 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
    if (!email || !password) return;
 
    return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+   if (!email || !password) return;
+
+   return await signInWithEmailAndPassword(auth, email, password);
 };
